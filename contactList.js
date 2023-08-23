c8d48bf529c38fbaf313bbc8aaba0b046e874b3e
JhonFDev <jhonfdeveloper@gmail.com>
2023-08-23 16:24:40 -0500

    actualizado de arrays a objetos

--- contactList.js
+++ contactList.js
@@ -1,42 +1,62 @@
 // Lista de contactos predefinidos
-const contactList = ["John florez", "Diana gomez"];
+let contactList = [
+  {
+    id: 1,
+    nombre: "John",
+    apellido: "Florez",
+    location: {
+      city: "Ocaña",
+      address: "mzn 2 - piso 4",
+    },
+  },
+  {
+    id: 2,
+    nombre: "Karoll",
+    apellido: "jalabe",
+    location: {
+      city: "Ocaña",
+      address: "altos de santana - calle 4 # 3-45",
+    },
+  },
+  
+];
 
 // Función para añadir un nuevo contacto a la lista
-function addContact(contactName) {
-  contactList.push(contactName);
+function aggContact(contactNew) {
+  contactList.push(contactNew);
 }
 
 // Función para borrar un contacto existente de la lista
-function deleteContact(contactName) {
-  const index = contactList.indexOf(contactName);
-  if (index !== -1) {
-    contactList.splice(index, 1);
-  } else {
-    console.log(`${contactName} not found in contacts.`);
-  }
+function eliminarContacto(contactoAEliminar) {
+  let nuevaLista = contactList.filter(
+    (contacto) => contacto.id !== contactoAEliminar
+  );
+  contactList = nuevaLista;
 }
 
 // Función para imprimir los contactos presentes en la lista
-function printContacts() {
-  if (contactList.length > 0) {
-    console.log("Contact List:");
-    contactList.forEach(contact => {
-      console.log(contact);
-    });
-  } else {
-    console.log("Contact list is empty.");
-  }
+function imprimirlistadecontactos() {
+  console.log(contactList);
 }
 
-// Añadir un nuevo contacto
-addContact("Alice Johnson");
+let contactNew={
+  id: 3,
+  nombre: "Nico",
+  apellido: "Robin",
+  location: {
+    city: "Ohara",
+    address: "mzn 1 - piso 1",
+  },
+}
 
-// Imprimir contactos después de añadir uno nuevo
-printContacts();
-
+console.log(contactNew.nombre)
 // Borrar un contacto existente
-deleteContact("John florez");
 
+// Añadir un nuevo contacto
+eliminarContacto(2)
+aggContact(contactNew);
+// Imprimir contactos después de añadir uno nuevo
+// imprimirlistadecontactos();
+console.log(contactList)
 // Imprimir contactos después de borrar uno
-printContacts();
-
+// printContacts();
